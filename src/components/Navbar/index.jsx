a93b73eec7e08aea894159f450f39f96038b18a4
Dhananjayan D <dhananjayan.dayalan@gmail.com>
2022-12-23 23:12:51 +0530

    Overlay setup for sidebar using createPortal

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+import classes from './Navbar.module.scss';
+
+const overlayDOM = document.getElementById('overlay');
+
+const Sidebar = ({ handleMenu, handleRoute }) => {
+  const [close, setClose] = useState(false);
+
+  return <Fragment></Fragment>;
+};
 
 const Navbar = ({ handleRoute }) => {
+  const [menu, setMenu] = useState(false);
+
+  const menuHandler = () => {
+    setMenu(!menu);
+  };
+
   return (
-    <div>
-      <nav>
-        <ul>
-          <li onClick={() => handleRoute("")}>Home</li>
-          <li onClick={() => handleRoute("destination")}>Destination</li>
-          <li onClick={() => handleRoute("crew")}>Crew</li>
-          <li onClick={() => handleRoute("technology")}>Technology</li>
-        </ul>
-      </nav>
+    <div className={classes.navbar}>
+      <img src='./assets/shared/logo.svg' alt='space tour logo' />
+      <div className={classes['nav-items']}>
+        {!menu && (
+          <img
+            src='./assets/shared/icon-hamburger.svg'
+            role='button'
+            onClick={menuHandler}
+            aria-pressed={menu}
+            id={classes['hamburger-logo']}
+          />
+        )}
+        {menu && <Sidebar handleMenu={menuHandler} handleRoute={handleRoute} />}
+      </div>
     </div>
   );
 };
