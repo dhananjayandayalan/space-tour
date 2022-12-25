48e60664e41aab3f9778b82506270d896bd7036a
Dhananjayan D <dhananjayan.dayalan@gmail.com>
2022-12-25 19:22:13 +0530

    Destination without Tabs

--- src/pages/Destination/index.jsx
+++ src/pages/Destination/index.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from 'react';
+
+import classes from './Destination.module.scss';
+import { Tabs } from '../../components';
 
 const Destination = ({ data }) => {
-  return <div>{JSON.stringify(data)}</div>;
+  const [index, setIndex] = useState(0);
+  const value = data[index];
+
+  const names = data.map((item) => item.name);
+
+  const indexHandler = (index) => {
+    setIndex(index);
+  };
+
+  return (
+    <div className={classes.destination}>
+      <h3>
+        <span>01</span> Pick your Destination
+      </h3>
+      <figure>
+        <picture>
+          <source type='image/webp' srcSet={value.images.webp} />
+          <img src={value.images.png} alt={`Image of ${names[index]}`} />
+        </picture>
+        <figcaption>
+          {/* <Tabs index={index} handleTabs={indexHandler} data={names} /> */}
+          <h1>{value.name}</h1>
+          <p>{value.description}</p>
+          <hr />
+          <div className={classes.facts}>
+            <div className={classes.fact}>
+              <p>Avg. Distance</p>
+              <h3>{value.distance}</h3>
+            </div>
+            <div className={classes.fact}>
+              <p>Est. Travel Time</p>
+              <h3>{value.travel}</h3>
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
 };
 
 export default Destination;
